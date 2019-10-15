a4346406eb77fdc6bb1e7e9bd5b20ce278942503
Elmer C <elmer.galo@outlook.com>
2019-10-14 20:43:30 -0400

    Add list of topics and created buttons with each topic

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,12 @@
 $(function(){
 
+    var topics = ["astronomy", "travel", "science", "soccer", "web development", "comic-con"];
+
+    topics.forEach(function(topic){
+        var button = $("<button>").text(topic);
+        $(".gif-button-area").append(button);
+    });
+
     var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=tJ7NkC6kpacg7ss87ZS2d3E4FRKb6CBg&q=tech&limit=10";
 
     $.ajax({
