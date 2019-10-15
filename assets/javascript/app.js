42081178f332df8c9fd6ed8e336b91ddb52ae592
Elmer C <elmer.galo@outlook.com>
2019-10-14 22:00:00 -0400

    Rename and add class names to HTML element for better identification

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,7 +4,7 @@ $(function(){
 
     topics.forEach(function(topic){
         var button = $("<button>").text(topic);
-        $(".gif-button-area").append(button);
+        $(".gif-buttons").append(button);
     });
 
     var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=tJ7NkC6kpacg7ss87ZS2d3E4FRKb6CBg&q=tech&limit=10";
